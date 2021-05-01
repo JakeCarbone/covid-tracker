369de4e30736e635e8bec3552eb0894c57a5ff3d
Jake Carbone <49517673+JakeCarbone@users.noreply.github.com>
2021-05-01 16:40:26 +0100

    starting to move stuff across

---
+++ src/streamgraph.js
@@ -0,0 +1,123 @@
+// Imports
+import * as d3 from "d3";
+
+////////////////////////////////////////////////////////
+//////////////////// Setup /////////////////////////////
+////////////////////////////////////////////////////////
+
+let margin = ({ top: 100, right: 40, bottom: 100, left: 40 });
+
+let height = 500;
+
+let palette = ["#73a2ab", "#bc586e", "#c79164", "#1c3c4a", "#dbbfc5", "#924f5d", "#cc7e72", "#172121", "#172121"];
+
+input_data = d3.csv('https://raw.githubusercontent.com/JakeCarbone/covid-tracker/master/data/gdp_percentile_data.csv');
+
+
+// https://github.com/JakeCarbone/covid-tracker/blob/master/data/gdp_percentile_data.csv
+////////////////////////////////////////////////////////
+/////////////////////// Functions //////////////////////////
+////////////////////////////////////////////////////////
+
+// Hover functions
+let hover = function(svg, path) {
+
+  svg
+      .on("mousemove", moved)
+      .on("mouseenter", entered)
+      .on("mouseleave", left);
+
+  const line = svg.append("g")
+       .attr("display", "none");
+
+  line.append("g")
+      .selectAll("line")
+      .data(series)
+      .join("line")
+      .attr("class", "cursor-line")
+      .attr("fill", "#fff")
+      .attr("stroke-width", 0.5)
+      .attr("x1", 10)
+      .attr("y1", height - 20)
+      .attr("x2", 10)
+      .attr("y2", 10);
+
+  line.append("text")
+      .attr("class", "text-year")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("x", 0)
+      .attr("y", 26)
+      .attr("transform", "rotate(-90 20 20) translate(0, -20)");
+
+  line.append("g")
+      .selectAll("text")
+      .data(series)
+      .join("text")
+      .attr("class", "text-genre")
+      .attr("font-weight", "bold")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("x", 5)
+      .attr("y", height - 43)
+      .attr("transform", "rotate(-90 10 460)");
+
+  line.append("g")
+      .selectAll("text")
+      .data(data)
+      .join("text")
+      .attr("class", "text-rating")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("x", 14)
+      .attr("y", height - 36);
+
+  function moved(event) {
+
+    event.preventDefault();
+    const pointer = d3.pointer(event, this);
+
+    const xm = x.invert(pointer[0]);
+    const ym = y.invert(pointer[1]);
+    const genre = d3.select(event.target).attr("data-genre");
+
+    line.attr("transform", `translate(${pointer[0]},0)`)
+        .style("visibility", "inherit");
+
+    line.select(".text-year")
+      .text(parseInt(xm));
+
+    line.selectAll(".text-rating")
+      .text(d => d.pub_year === parseInt(xm) ? d[genre] + " reviews" : "");
+
+    line.selectAll(".text-genre")
+      .style("visibility", "hidden")
+      .filter(d => d.key === genre)
+      .style("visibility", "inherit")
+      .attr("fill", d => d.key === genre ? color(d.key) : "#000")
+      .text(genre);
+
+    line.selectAll(".cursor-line")
+      .attr("stroke", "fff0")
+      .filter(d => d.key === genre)
+      .attr("stroke", d => d.key === genre ? color(d.key) : "#fff0")
+
+    path.attr("opacity", d => d.key === genre ? 1 : 0.2);
+
+    console.log(genre);
+
+    if (genre === null) {
+      path.attr("opacity", 1);
+      line.style("visibility", "hidden");
+    }
+  }
+
+  function entered() {
+    line.attr("display", null);
+  }
+
+  function left() {
+    line.attr("display", "none");
+  }
+};
+
